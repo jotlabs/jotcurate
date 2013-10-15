4b61c1b14d0c9cfde5111d097d64ec50a2f049f3
Mike Davies <isofarro2@gmail.com>
2013-10-15 20:44:01 +0100

    Quick mysql model for a simple text note

---
+++ etc/schema/notes-model.sql
@@ -0,0 +1,11 @@
+--
+-- Model: Note
+--
+
+CREATE TABLE IF NOT EXISTS `notes` (
+    _id         INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title       VARCHAR(255),
+    slug        VARCHAR(255),
+    text        TEXT,
+    dateAdded   DATETIME
+);
