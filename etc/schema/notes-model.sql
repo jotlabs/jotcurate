6c28ee41a93cd23c2598cc852a6e63f1b80e9f1b
Mike Davies <isofarro2@gmail.com>
2013-10-15 21:24:56 +0100

    Make slug unique field

--- etc/schema/notes-model.sql
+++ etc/schema/notes-model.sql
@@ -5,7 +5,7 @@
 CREATE TABLE IF NOT EXISTS `notes` (
     _id         INTEGER PRIMARY KEY AUTO_INCREMENT,
     title       VARCHAR(255),
-    slug        VARCHAR(255),
+    slug        VARCHAR(255) UNIQUE,
     text        TEXT,
     dateAdded   DATETIME
 );
