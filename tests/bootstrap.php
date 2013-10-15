ef5bcde48811074a165f9fcdb889481b13a3fd9a
Mike Davies <isofarro2@gmail.com>
2013-10-15 21:33:52 +0100

    Point UNITTEST_DB_PATH to the directory containing the unittest sqlite databases

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 # Constant for path to sqlite databases for unit tests
-define('UNITTEST_DB_PATH', dirname(__FILE__) . '/unittest-db/');
+define('UNITTEST_DB_PATH', dirname(dirname(__FILE__)) . '/var/unittest-db/');
 
 # Use composer's autoloader
 require 'vendor/autoload.php';
