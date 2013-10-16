99e40aeb5217c5d1446c910db3e49be89fb2555c
Mike Davies <isofarro2@gmail.com>
2013-10-16 18:29:36 +0100

    Copy the unittest database to the /tmp directory, and return that as the sqlite data source for unit tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 # Constant for path to sqlite databases for unit tests
 define('UNITTEST_DB_PATH', dirname(dirname(__FILE__)) . '/var/unittest-db/');
 define('UNITTEST_DB',      UNITTEST_DB_PATH . 'jotcurate.db');
+define('TMP_DB_PATH',      dirname(dirname(__FILE__)) . '/tmp/unittest-db/');
 
 # Use composer's autoloader
 require 'vendor/autoload.php';
@@ -9,8 +10,14 @@ require 'vendor/autoload.php';
 class UnitTestUtils {
 
     function createDsn() {
-        // TODO: Break this out into copying the starting point database and using that
-        return 'sqlite:' . UNITTEST_DB;
+        if (!file_exists(TMP_DB_PATH)) {
+            mkdir(TMP_DB_PATH);
+        }
+
+        $tmpDb = TMP_DB_PATH . 'unittest-' . time() . '.db';
+        copy(UNITTEST_DB, $tmpDb);
+
+        return 'sqlite:' . $tmpDb;
     }
 
 }
