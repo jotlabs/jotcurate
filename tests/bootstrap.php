796bba28bccc80170e195edd2ea11ac1f699477e
Mike Davies <isofarro2@gmail.com>
2013-10-16 07:45:44 +0100

    Utility methods for setting up unittest ready sqlite databases

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,18 @@
 <?php
 # Constant for path to sqlite databases for unit tests
 define('UNITTEST_DB_PATH', dirname(dirname(__FILE__)) . '/var/unittest-db/');
+define('UNITTEST_DB',      UNITTEST_DB_PATH . 'jotcurate.db');
 
 # Use composer's autoloader
 require 'vendor/autoload.php';
+
+class UnitTestUtils {
+
+    function createDsn() {
+        // TODO: Break this out into copying the starting point database and using that
+        return 'sqlite:' . UNITTEST_DB;
+    }
+
+}
 
 ?>
