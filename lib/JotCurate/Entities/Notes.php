c04dc4fd411c60fd522434638bac68385817d0b7
Mike Davies <isofarro2@gmail.com>
2013-10-16 07:47:21 +0100

    Test and implement getting a note by slug, and deferring the creation of sqlite database to unittest utilities

--- lib/JotCurate/Entities/Notes.php
+++ lib/JotCurate/Entities/Notes.php
@@ -8,7 +8,8 @@ class Notes extends Entity {
 
     static $models = array(
         'notes' => array(
-            'INSERT' => 'INSERT INTO `notes` VALUES(NULL, :title, :slug, :text, NOW());'
+            'INSERT' => 'INSERT INTO `notes` VALUES(NULL, :title, :slug, :text, NOW());',
+            'bySlug' => 'SELECT * FROM `notes` WHERE slug = :slug LIMIT 0,1'
         )
     );
 
@@ -18,6 +19,12 @@ class Notes extends Entity {
             'title' => $note->title,
             'slug'  => Slug::slugify($note->title),
             'text'  => $note->text
+        ));
+    }
+
+    public function getBySlug($slug) {
+        return $this->dataSource->findOne('notes', 'bySlug', 'JotCurate\Models\Note', array(
+            'slug' => $slug
         ));
     }
 }
--- tests/JotCurate/Entites/NotesTest.php
+++ tests/JotCurate/Entites/NotesTest.php
@@ -5,10 +5,11 @@ use JotCurate\Entities\Notes;
 
 class NotesTest extends PHPUnit_Framework_TestCase {
     protected $notes;
-    protected $dsn = 'sqlite:{DB_PATH}jotcurate.db';
+    //protected $dsn = 'sqlite:{DB_PATH}jotcurate.db';
 
     public function setUp() {
-        $dsn = preg_replace('/\{DB_PATH\}/', UNITTEST_DB_PATH, $this->dsn);
+        //$dsn = preg_replace('/\{DB_PATH\}/', UNITTEST_DB_PATH, $this->dsn);
+        $dsn = UnitTestUtils::createDsn();
         $this->notes = Notes::getInstance();
         $this->notes->setDataSource(new PdoDataSource($dsn));
     }
@@ -24,6 +25,18 @@ class NotesTest extends PHPUnit_Framework_TestCase {
         $this->assertTrue(is_array(Notes::$models));
         $this->assertTrue(is_array(Notes::$models['notes']));
         $this->assertTrue(is_string(Notes::$models['notes']['INSERT']));
+    }
+
+    public function testDataSource() {
+    }
+
+    public function testGetBySlug() {
+        $note = $this->notes->getBySlug('unit-test-note');
+        $this->assertNotNull($note);
+        $this->assertTrue(!empty($note));
+        $this->assertTrue(is_a($note, 'JotCurate\Models\Note'));
+        $this->assertTrue(is_a($note, 'JotContent\Model'));
+
     }
 
 }
