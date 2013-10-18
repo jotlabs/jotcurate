a2eb5c370bcc270ef6b68c2bec9c9cbac916cc19
Mike Davies <isofarro2@gmail.com>
2013-10-18 07:45:46 +0100

    Application wide configuration, supports multiple environments: prod, staging, dev

---
+++ etc/app/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = array(
+    'DEV' => array(
+        'domain'        => 'jotcurate.dev',
+        'datasources'   => array(
+            '__DEFAULT__'   => array(
+                'datasource' => 'mysql:host=localhost;dbname=jotcurate', 
+                'username'   => 'webdev', 
+                'password'   => 'webdev'
+            )
+        )
+    )
+);
+
+?>
