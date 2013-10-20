4298343fb46940db6d1a0cdd2b1b6dfe8ec45d31
Mike Davies <isofarro2@gmail.com>
2013-10-20 14:33:31 +0100

    routes for simple list, add, edit, and view notes

--- etc/routes/notes.php
+++ etc/routes/notes.php
@@ -2,8 +2,32 @@
 
 $slim->get('/', function() use($slim, $app) {
     $slim->render('layout.tpl', array(
-        'content' => ''
+        'content' => 'Show all notes'
     ));
+})->name('allNotes');
+
+
+$slim->post('/', function() use($slim, $app) {
+    $slim->render('layout.tpl', array(
+        'content' => "Add new note"
+    ));
+    
+});
+
+
+$slim->get('/:slug', function($slug) use($slim, $app) {
+    $slim->render('layout.tpl', array(
+        'content' => "Show note: $slug"
+    ));
+
+});
+
+
+$slim->put('/:slug', function($slug) use($slim, $app) {
+    $slim->render('layout.tpl', array(
+        'content' => "Update note: $slug"
+    ));
+
 });
 
 
