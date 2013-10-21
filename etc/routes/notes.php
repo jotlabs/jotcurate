d4d514c947c43684533cf388a2f0dc8cd7b9bdef
Mike Davies <isofarro2@gmail.com>
2013-10-21 07:59:52 +0100

    Dump out incoming params on save note endpoint

--- etc/routes/notes.php
+++ etc/routes/notes.php
@@ -8,11 +8,11 @@ $slim->get('/', function() use($slim, $app) {
 
 
 $slim->post('/', function() use($slim, $app) {
-    $slim->render('layout.tpl', array(
-        'content' => "Add new note"
-    ));
-    
-});
+    $params = $slim->request()->post();
+    echo '<h1>Adding a new note:</h1>';
+    echo '<pre>Params: '; print_r($params); echo '</pre>';
+
+})->name('saveNote');
 
 
 $slim->get('/:slug', function($slug) use($slim, $app) {
